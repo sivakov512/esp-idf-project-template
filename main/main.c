57d08eba2b2bca4819a0e333b30dd4bc5ab9ec1b
Nikita Sivakov <sivakov512@icloud.com>
2025-11-27 01:54:01 +0100

    Use slog logger instead of embedlibs

--- main/main.c
+++ main/main.c
@@ -1,25 +1,28 @@
 #include "app.h"
-#include <embedlibs/log.h>
+#include <slog.h>
+#include <slog/platform/esp.h>
 #include <stdint.h>
 
 #define TAG "main"
 
 void app_main() {
-  LOGI("Initializing app...");
+  slog_esp_init();
+
+  SLOGI("Initializing app...");
   esp_err_t err = app_init();
   if (err != ESP_OK) {
-    LOGE("Failed to initialize app, %s", esp_err_to_name(err));
+    SLOGE("Failed to initialize app, %s", esp_err_to_name(err));
     goto err;
   }
 
-  LOGI("Running app...");
+  SLOGI("Running app...");
   err = app_run();
   if (err != ESP_OK) {
-    LOGE("Failed to run app, %s", esp_err_to_name(err));
+    SLOGE("Failed to run app, %s", esp_err_to_name(err));
     goto err;
   }
 
-  LOGI("App started");
+  SLOGI("App started");
 
 err:
   ESP_ERROR_CHECK(err);
