0287cacb5e6d3afe4226fd4e7698f6aa8c819660
Nikita Sivakov <sivakov512@icloud.com>
2026-03-03 19:00:56 +0100

    Change indentation

--- components/app/app.c
+++ components/app/app.c
@@ -1,11 +1,11 @@
 #include "app.h"
 
 esp_err_t app_init(app_ctx_t *ctx) {
-  (void) ctx;
-  return ESP_OK;
+    (void) ctx;
+    return ESP_OK;
 }
 
 esp_err_t app_run(app_ctx_t *ctx) {
-  (void) ctx;
-  return ESP_OK;
+    (void) ctx;
+    return ESP_OK;
 }
--- main/main.c
+++ main/main.c
@@ -5,26 +5,26 @@
 SLOG_REGISTER("main");
 
 void app_main() {
-  SLOGI("Initializing app...");
+    SLOGI("Initializing app...");
 
-  static app_ctx_t ctx = {};
+    static app_ctx_t ctx = {};
 
-  esp_err_t err = app_init(&ctx);
-  if (err != ESP_OK) {
-    SLOGE("Failed to initialize app, %s", esp_err_to_name(err));
-    goto err;
-  }
+    esp_err_t err = app_init(&ctx);
+    if (err != ESP_OK) {
+        SLOGE("Failed to initialize app, %s", esp_err_to_name(err));
+        goto err;
+    }
 
-  SLOGI("Running app...");
-  err = app_run(&ctx);
-  if (err != ESP_OK) {
-    SLOGE("Failed to run app, %s", esp_err_to_name(err));
-    goto err;
-  }
+    SLOGI("Running app...");
+    err = app_run(&ctx);
+    if (err != ESP_OK) {
+        SLOGE("Failed to run app, %s", esp_err_to_name(err));
+        goto err;
+    }
 
-  SLOGI("App started");
-  return;
+    SLOGI("App started");
+    return;
 
 err:
-  ESP_ERROR_CHECK(err);
+    ESP_ERROR_CHECK(err);
 }
